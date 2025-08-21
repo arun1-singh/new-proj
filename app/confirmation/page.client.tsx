fb9bd599748a2f73bb95975c30416fc719ca2771
arun1-singh <arunsingh17683@gmail.com>
2025-08-21 19:18:39 +0530

    final updates

---
+++ app/confirmation/page.client.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import ConfirmationPage from "./ConfirmationClient";
+
+export default function Page() {
+  return <ConfirmationPage />;
+}
--- app/confirmation/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/signup/page.tsx
-import ConfirmationPage from "./ConfirmationClient";
-
-export const dynamic = "force-dynamic"; // ✅ important
-
-export default function Page() {
-  return <ConfirmationPage />;
-}
